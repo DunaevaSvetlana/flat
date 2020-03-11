1dee8dd6b77929319cf703aac4575b88ce978e08
DunaevaSvetlana <solnste1993@mail.ru>
2020-03-12 00:59:50 +0400

    Initial commit

---
+++ src/Flat.java
@@ -0,0 +1,14 @@
+public class Flat {
+    public static void main(String[] args) {
+        final int costFlat = 1_800_000;
+        final double persentFistPay = 0.3;
+
+        final double fistPay = costFlat * persentFistPay;
+
+        System.out.println("costFlat = " + costFlat);
+        System.out.println("persentFistPay = " + persentFistPay);
+        System.out.println("fistPay = " + fistPay);
+
+
+    }
+}
